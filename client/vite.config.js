de9ef819e7a755687cebce78a4b3d495238503ea
MADDINALA SANDHYA LAKSHMI <sandhyamaddinala812@gmail.com>
2026-07-19 21:45:41 +0530

    Fix Vercel base path

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,5 +4,11 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/FUTURE_FS_02/',
+  import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+export default defineConfig({
+  plugins: [react()],
+  base: '/',
+})
 })
